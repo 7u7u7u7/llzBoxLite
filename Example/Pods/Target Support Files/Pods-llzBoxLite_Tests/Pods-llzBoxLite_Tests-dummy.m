6defabcad22fc8bea3162b4bed361b28dc1ca8ee
liaozhe <1165302931@qq.com>
2022-04-07 16:49:22 +0800

    init

---
+++ Example/Pods/Target Support Files/Pods-llzBoxLite_Tests/Pods-llzBoxLite_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_llzBoxLite_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_llzBoxLite_Tests
+@end
---
+++ Example/Pods/Target Support Files/llzBoxLite/llzBoxLite-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_llzBoxLite : NSObject
+@end
+@implementation PodsDummy_llzBoxLite
+@end
